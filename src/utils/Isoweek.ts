382364efe6f9217d107dbf79093f0991ba9ea45a
kikogp <fgonzalezp3@alumnos.nebrija.es>
2026-07-09 19:55:49 +0200

    Vercel intento 2

--- src/utils/Isoweek.ts
+++ src/utils/Isoweek.ts
@@ -3,7 +3,7 @@
 export const getIsoWeek = (date: Date = new Date()): { year: number; weekNumber: number } => {
     const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
     const dayNum = d.getUTCDay() || 7; // domingo = 7, no 0
-    d.setUTCDate(d.getUTCDate() + 4 - dayNum); // mover al jueves de esa semana
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum); 
     const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
     const weekNumber = Math.ceil(((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7);
     return { year: d.getUTCFullYear(), weekNumber };
